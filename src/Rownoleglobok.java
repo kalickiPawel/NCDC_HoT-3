d881660ce8467a6eb9c085af1054926bd27b20c5
Pawel Kalicki <kalicki.paul@icloud.com>
2018-05-25 00:54:15 +0200

    Initial Class Rownoleglobok

---
+++ src/Rownoleglobok.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Rownoleglobok {
+
+	public static void main (String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		String data = input.nextLine();
+		String delims = "[ ]+";
+		String[] tokens = data.split(delims);
+			
+		int shift=0;
+		int width = Integer.parseInt(tokens[0]);
+		int height = Integer.parseInt(tokens[1]);
+		int kick = Integer.parseInt(tokens[2]);
+			
+		if (width>0 && width<=100 && height>0 && height<=100 && kick<=100 && kick>=-100)
+		{
+			int kickMinus=kick*-1;
+			int shiftMinus = height*kickMinus;
+					
+			for (int j=0; j<height; j++)
+			{
+				if(kick>=0) 
+				{						
+					for (int m=0; m<shift; m++)
+						System.out.print(" ");
+				}
+				else 
+				{
+					for (int m=shiftMinus-kickMinus; m>0; m--)
+						System.out.print(" ");
+				}
+					
+				for (int i=0; i<width; i++) 				
+					System.out.print("*");
+						
+				System.out.println();
+						
+				if(kick>=0)
+					shift += kick;
+				else
+					shiftMinus -= kickMinus;
+			}
+		}
+	}
+}
