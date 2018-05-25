1b4005ff0e21be6a466bb86242c0ca9c8dfec348
Pawel Kalicki <kalicki.paul@icloud.com>
2018-05-25 15:17:58 +0200

    Added if token.length==3

--- src/Rownoleglobok.java
+++ src/Rownoleglobok.java
@@ -9,39 +9,42 @@ public class Rownoleglobok
 		String data = input.nextLine();
 		String delims = "[ ]+";
 		String[] tokens = data.split(delims);
-			
-		int shift=0;
-		int width = Integer.parseInt(tokens[0]);
-		int height = Integer.parseInt(tokens[1]);
-		int kick = Integer.parseInt(tokens[2]);
-			
-		if (width>0 && width<=100 && height>0 && height<=100 && kick<=100 && kick>=-100)
+		
+		if(tokens.length==3)
 		{
-			int kickInv=kick*-1;
-			int shiftMax = height*kickInv;
-					
-			for (int j=0; j<height; j++)
+			int shift=0;
+			int width = Integer.parseInt(tokens[0]);
+			int height = Integer.parseInt(tokens[1]);
+			int kick = Integer.parseInt(tokens[2]);
+				
+			if (width>0 && width<=100 && height>0 && height<=100 && kick<=100 && kick>=-100)
 			{
-				if(kick>=0) 
-				{						
-					for (int m=0; m<shift; m++)
-						System.out.print(" ");
-				}
-				else 
+				int kickInv=kick*-1;
+				int shiftMax = height*kickInv;
+						
+				for (int j=0; j<height; j++)
 				{
-					for (int m=shiftMax-kickInv; m>0; m--)
-						System.out.print(" ");
+					if(kick>=0) 
+					{						
+						for (int m=0; m<shift; m++)
+							System.out.print(" ");
+					}
+					else 
+					{
+						for (int m=shiftMax-kickInv; m>0; m--)
+							System.out.print(" ");
+					}
+						
+					for (int i=0; i<width; i++) 				
+						System.out.print("*");
+							
+					System.out.println();
+							
+					if(kick>=0)
+						shift += kick;
+					else
+						shiftMax -= kickInv;
 				}
-					
-				for (int i=0; i<width; i++) 				
-					System.out.print("*");
-						
-				System.out.println();
-						
-				if(kick>=0)
-					shift += kick;
-				else
-					shiftMax -= kickInv;
 			}
 		}
 	}
